be2f5249199dc53a4e037f57897494a9c7d9be17
Александра Цепкова <aleksandra.tsepkova@emergingtravel.com>
2025-10-22 22:34:49 +0200

    Add class Context + CI workflow

--- terminal-emulator/src/context.py
+++ terminal-emulator/src/context.py
@@ -2,20 +2,54 @@ import abc
 
 
 class ContextProtocol(abc.ABC):
-    def add_unscoped_param(name: str, value: str) -> None:
+    def add_unscoped_param(self, name: str, value: str) -> None:
         ...
 
-    def add_scoped_param(name: str, value: str) -> None:
+    def add_scoped_param(self, name: str, value: str) -> None:
         ...
 
-    def exit_scope() -> None:
+    def exit_scope(self) -> None:
         ...
     
-    def get_env() -> dict[str, str]:
+    def get_env(self) -> dict[str, str]:
         ...
     
-    def get_value(name: str) -> str:
+    def get_value(self, name: str) -> str:
         ...
 
-    def populate_values(template: str) -> str:
+    def populate_values(self, template: str) -> str:
         ...
+
+
+
+class Context(ContextProtocol):
+    def __init__(self):
+        self.unscoped_params: dict[str, str] = {}
+        self.scoped_params: dict[str, str] = {}
+
+    def add_unscoped_param(self, name: str, value: str) -> None:
+        """Add an environment variable. Cannot be removed by scope."""
+        self.unscoped_params[name] = value
+
+    def add_scoped_param(self, name: str, value: str) -> None:
+        """Add a temporary variable valid until exit_scope() is called."""
+        self.scoped_params[name] = value
+
+    def exit_scope(self) -> None:
+        """Remove all scoped variables from the current scope."""
+        self.scoped_params.clear()
+
+    def get_env(self) -> dict[str, str]:
+        """Return a dictionary of all current variables."""
+        return {**self.unscoped_params, **self.scoped_params}
+
+    def get_value(self, name: str) -> str:
+        """Return the value of a variable, or empty string if not found."""
+        return self.get_env().get(name, "")
+
+    def populate_values(self, template: str) -> str:
+        """Return template filled with variable values."""
+        updated_template = template
+        for k, v in {**self.unscoped_params, **self.scoped_params}.items():
+            updated_template = updated_template.replace(f'${k}', v)
+        return updated_template
