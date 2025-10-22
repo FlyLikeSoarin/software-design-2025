1a1fbaedca79e66faccdbb92e14e45a0104d6235
Александра Цепкова <aleksandra.tsepkova@emergingtravel.com>
2025-10-22 22:34:49 +0200

    Context first version + add gitignore

--- terminal-emulator/src/context.py
+++ terminal-emulator/src/context.py
@@ -2,20 +2,48 @@ import abc
 
 
 class ContextProtocol(abc.ABC):
-    def add_unscoped_param(name: str, value: str) -> None:
+    def add_unscoped_param(self, name: str, value: str) -> None:
         ...
 
-    def add_scoped_param(name: str, value: str) -> None:
+    def add_scoped_param(self, name: str, value: str) -> None:
         ...
 
-    def exit_scope() -> None:
+    def exit_scope(self) -> None:
         ...
     
-    def get_env() -> dict[str, str]:
+    def get_env(self) -> dict[str, str]:
         ...
     
-    def get_value(name: str) -> str:
+    def get_value(self, name: str) -> str:
         ...
 
-    def populate_values(template: str) -> str:
+    def populate_values(self, template: str) -> str:
         ...
+
+
+
+class Context(ContextProtocol):
+    def __init__(self):
+        self.unscoped_params: dict[str, str] = {}
+        self.scoped_params: dict[str, str] = {}
+
+    def add_unscoped_param(self, name: str, value: str) -> None:
+        self.unscoped_params[name] = value
+
+    def add_scoped_param(self, name: str, value: str) -> None:
+        self.scoped_params[name] = value
+
+    def exit_scope(self) -> None:
+        self.scoped_params.clear()
+
+    def get_env(self) -> dict[str, str]:
+        return {**self.unscoped_params, **self.scoped_params}
+
+    def get_value(self, name: str) -> str:
+        return {**self.unscoped_params, **self.scoped_params}.get(name, "")
+
+    def populate_values(self, template: str) -> str:
+        updated_template = template
+        for k, v in {**self.unscoped_params, **self.scoped_params}.items():
+            updated_template.replace(f'${k}', v)
+        return updated_template
