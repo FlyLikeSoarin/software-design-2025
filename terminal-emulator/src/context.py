2f94924b07f766e87a3d18e252bdde7f88fefb5a
Александра Цепкова <aleksandra.tsepkova@emergingtravel.com>
2025-10-22 22:40:27 +0200

    Add dosctring

--- terminal-emulator/src/context.py
+++ terminal-emulator/src/context.py
@@ -28,21 +28,27 @@ class Context(ContextProtocol):
         self.scoped_params: dict[str, str] = {}
 
     def add_unscoped_param(self, name: str, value: str) -> None:
+        """Add an environment variable. Cannot be removed by scope."""
         self.unscoped_params[name] = value
 
     def add_scoped_param(self, name: str, value: str) -> None:
+        """Add a temporary variable valid until exit_scope() is called."""
         self.scoped_params[name] = value
 
     def exit_scope(self) -> None:
+        """Remove all scoped variables from the current scope."""
         self.scoped_params.clear()
 
     def get_env(self) -> dict[str, str]:
+        """Return a dictionary of all current variables."""
         return {**self.unscoped_params, **self.scoped_params}
 
     def get_value(self, name: str) -> str:
+        """Return the value of a variable, or empty string if not found."""
         return {**self.unscoped_params, **self.scoped_params}.get(name, "")
 
     def populate_values(self, template: str) -> str:
+        """Return template filled with variable values."""
         updated_template = template
         for k, v in {**self.unscoped_params, **self.scoped_params}.items():
             updated_template.replace(f'${k}', v)
