42b9a827015744a419279a522badb022d6caf307
Eugene Vihrev <evgeny.vihrev@gmail.com>
2025-11-06 23:04:07 +0100

    Integrate builtins and fix CLI part 2

--- terminal-emulator/src/main.py
+++ terminal-emulator/src/main.py
@@ -1,11 +1,15 @@
-import src.context as context
-import src.executor as executor
-import src.io as io
+import src.context as context_lib
+import src.exceptions as exceptions_lib
+import src.executor as executor_lib
+import src.io as io_lib
 
 
-context = context.Context()
-executor = executor.Executor()
-io = io.IO(context, executor)
+context = context_lib.Context()
+executor = executor_lib.Executor()
+io = io_lib.IO(context, executor)
 
-while io.parse_command():
-    pass
+try:
+    while io.parse_command():
+        pass
+except exceptions_lib.ExitException:
+    print("Quitting...")
