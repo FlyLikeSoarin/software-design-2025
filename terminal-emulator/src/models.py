839cd941d7b0200b498c9e8e4192aaf8a50b30d1
Eugene Vihrev <evgeny.vihrev@gmail.com>
2025-11-06 23:04:07 +0100

    Integrate builtins and fix CLI part 2

--- terminal-emulator/src/models.py
+++ terminal-emulator/src/models.py
@@ -11,3 +11,10 @@ class Command:
 class Status:
     code: int  # Return code of last command or first command that failed
     index: int  # Index of command which return code is written above
+
+
+class ProcessResult:
+    returncode: int
+
+    def __init__(self, code: int):
+        self.returncode = code
